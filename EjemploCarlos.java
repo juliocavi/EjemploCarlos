f34400a4f8dc0531b86d0c7ff862f34583b7cdff
Julio Cachon Villadangos <juliocachonvi@gmail.com>
2018-02-05 16:31:05 +0100

    Añadido metodo hola chacho

---
+++ EjemploCarlos.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class EjemploCarlos here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class EjemploCarlos
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    public void holaMundo(){
+        System.out.println("Hola chacho");
+    }
+    
+    /**
+     * Constructor for objects of class EjemploCarlos
+     */
+    public EjemploCarlos()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
